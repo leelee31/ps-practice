db1e06404cdbdd8df75ad2ecb7f81eb074a98e21
leesumin <45748683+leelee31@users.noreply.github.com>
2023-06-16 14:40:51 +0900

    [level 2] Title: 구명보트, Time: 10.13 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int s = 0, e = people.length-1;
+        while (s <= e) {
+            if (people[s] + people[e] <= limit) {
+                s++;
+            }
+            answer++;
+            e--;
+        }
+        return answer;
+    }
+}
