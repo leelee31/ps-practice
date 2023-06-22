b301ba8c58b5254b4ea5aa20ece973796e69c8c6
leesumin <45748683+leelee31@users.noreply.github.com>
2023-06-22 12:53:09 +0900

    [level 2] Title: 모음 사전, Time: 13.69 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -4,15 +4,16 @@ class Solution {
     int order = 1;
     public int solution(String word) {
         Map<String, Integer> map = new HashMap<>();
-        makeIndex(0, map, new char[]{'A','E','I','O','U'}, "");
+        makeIndex(0, map, new char[]{'A','E','I','O','U'}, "", word);
         return map.get(word);
     }
     
-    public void makeIndex(int index, Map<String, Integer> map, char[] cArr, String str) {
+    public void makeIndex(int index, Map<String, Integer> map, char[] cArr, String str, String word) {
         if (index == cArr.length) return;
         for (int i=0; i<cArr.length; i++) {
             map.put(str + cArr[i], order++);
-            makeIndex(index + 1, map, cArr, str + cArr[i]);
+            if (map.containsKey(word)) return;
+            makeIndex(index + 1, map, cArr, str + cArr[i], word);
         }
     }
 }
