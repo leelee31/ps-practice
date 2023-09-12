1a1a4f3732d1b487478dc2312de7fe143a62d5ab
leesumin <45748683+leelee31@users.noreply.github.com>
2023-09-12 22:09:20 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] mem = new int[n + 1];
+
+        for (int i = 2; i < n + 1; i++) {
+            mem[i] = mem[i - 1] + 1;
+
+            if (i % 3 == 0)
+                mem[i] = Math.min(mem[i], mem[i / 3] + 1);
+            if (i % 2 == 0)
+                mem[i] = Math.min(mem[i], mem[i / 2] + 1);
+        }
+
+        System.out.println(mem[n]);
+    }
+}
